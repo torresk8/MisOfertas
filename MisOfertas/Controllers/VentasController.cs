3edc9cde9f68fdb88166b0e2a418f06254cb861a
torresk8 <31326033+torresk8@users.noreply.github.com>
2017-12-09 12:15:48 -0300

    Rol encargado tienda

--- MisOfertas/Controllers/VentasController.cs
+++ MisOfertas/Controllers/VentasController.cs
@@ -10,7 +10,7 @@ using System.Web.Mvc;
 
 namespace MisOfertas.Controllers
 {
-    [Authorize(Roles = "crearOferta")]    
+    [Authorize(Roles = "crearOferta,encargadoTienda")]    
     public class VentasController : Controller
     {
         // GET: Ventas
